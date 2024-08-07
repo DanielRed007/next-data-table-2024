4b6542997c4a815b3db0cd2ffe65dd48da6aebe8
Daniel Mesu <danielrojo1927@gmail.com>
2024-08-07 21:57:11 +0100

    improving error boundary strategy

--- app/api/info/route.ts
+++ app/api/info/route.ts
@@ -1,22 +1,24 @@
-import connectToDatabase, { seedDatabase } from "@/app/lib/mongoose";
+import connectToDatabase from "@/app/lib/mongoose";
 import { NextResponse } from "next/server";
 
 import UserSchema from "../../models/user";
 
 export async function GET() {
   try {
-    // await connectToDatabase().then(() => seedDatabase());
     await connectToDatabase();
 
     const dataTable = await UserSchema.find().limit(20);
 
-    if (dataTable) {
+    if (dataTable.length > 0) {
       return NextResponse.json(dataTable, { status: 200 });
     } else {
       return NextResponse.json({ msg: "No Profiles Found" }, { status: 404 });
     }
-  } catch (error) {
+  } catch (error: any) {
     console.error("Database connection or query error:", error);
-    return NextResponse.json({ msg: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { msg: "Internal Server Error", error: error },
+      { status: 500 }
+    );
   }
 }
--- app/redux/infoSlice.ts
+++ app/redux/infoSlice.ts
@@ -23,9 +23,6 @@ export const fetchInfo = createAsyncThunk(
         headers: { "Content-Type": "application/json" },
       });
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
       const data = await response.json();
       return data;
     } catch (error: any) {
